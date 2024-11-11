34d88cde7796717155cb21eb8e025dfc1f183354
pratik2405 <shinde7388pratik@gmail.com>
2024-11-11 11:58:54 +0530

    Add project files.

---
+++ StringBuilderDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderDemo
+{
+    internal class Program
+    {
+        //StringBuilder is a mutable class (change can happen at the same memory location)
+        //We need add System.Text namespace
+
+        static void Main(string[] args)
+        {
+            StringBuilder sb=new StringBuilder("Hello");
+            Console.WriteLine(sb);
+            Console.WriteLine("Capacity"+sb.Capacity);
+            Console.WriteLine("Length"+sb.Length);
+
+            //sb.Remove(2, 2);
+            //Console.WriteLine(sb);
+            //Console.WriteLine("Capacity" + sb.Capacity);
+            //Console.WriteLine("Length" + sb.Length);
+
+            //sb.Clear();
+            //Console.WriteLine(sb);
+            //Console.WriteLine("Capacity" + sb.Capacity);
+            //Console.WriteLine("Length" + sb.Length);
+
+            sb.Append(" Good morning");
+            Console.WriteLine(sb);
+            Console.WriteLine("Capacity" + sb.Capacity);
+            Console.WriteLine("Length" + sb.Length);
+
+            sb.Append(" All ");
+            Console.WriteLine(sb);
+            Console.WriteLine("Capacity" + sb.Capacity);
+            Console.WriteLine("Length" + sb.Length);
+
+            //sb.Remove(2, 2);
+            //Console.WriteLine(sb);
+            //Console.WriteLine("Capacity" + sb.Capacity);
+            //Console.WriteLine("Length" + sb.Length);
+
+            sb.Replace("All", "Everyone");
+            Console.WriteLine(sb);
+            Console.WriteLine("Capacity" + sb.Capacity);
+            Console.WriteLine("Length" + sb.Length);
+
+            //copy 
+            char[] ch2 = new char[5];
+            sb.CopyTo(1,ch2,0,5);
+            string str1=string.Join(" ",ch2);
+            Console.WriteLine(str1);
+
+
+        }
+    }
+}
---
+++ StringBuilderDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StringBuilderDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringBuilderDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d338d1d5-ebc1-4969-a7a5-c18afe3c2dcc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
